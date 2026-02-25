af9f47afa76fc753de727653402e2f4ed104669d
Mahmudjon <mrfx404@gmail.com>
2026-02-25 21:09:59 +0500

    Yangilanish3

--- Bitiruv/main/tests.py
+++ Bitiruv/main/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+# for i in range(1,101):
+#     print(i)
+# if i % 3 == 0 and i % 5 == 0:
+#     print("FizzBuzz")
+# elif i % 3 == 0:
+#     print("Fizz")
+# elif i % 5 == 0:
+#     print("Buzz")
 
-# Create your tests here.
+text = 'salom'
+text[::-1]
+print(text[::-1])
